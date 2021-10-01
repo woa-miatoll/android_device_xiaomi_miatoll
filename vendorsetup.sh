566c7d0eaa7d96f9ed21eb68b487d3094751b1a3
Darth9 <reyprimerjefe@gmail.com>
2021-10-01 17:12:29 +0100

    Permit hiding the notch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -42,7 +42,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export OF_USE_GREEN_LED=0
-
+	export OF_HIDE_NOTCH=1
 	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
