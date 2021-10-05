dc67beb0aeb50638255d4e99240914824df0aac3
Darth9 <reyprimerjefe@gmail.com>
2021-10-05 21:49:46 +0100

    Update the notes on what this build var is doing

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -76,7 +76,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
 	export OF_SPLASH_MAX_SIZE=104
 
-	# run a process after formatting data to work-around MTP issues
+	# run a process after formatting data to recreate /data/media/0 (only when forced-encryption is being disabled)
 	export OF_RUN_POST_FORMAT_PROCESS=1
 
 	# let's see what are our build VARs
