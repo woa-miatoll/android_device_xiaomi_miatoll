dafd9214b9e8d320d91054fe22af8bc229e6721e
Darth9 <reyprimerjefe@gmail.com>
2022-03-12 23:43:17 +0000

    Some minor reorganisation

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
+# fscrypt policy
 TW_USE_FSCRYPT_POLICY := 1
 
 # dynamic partition stuff
@@ -55,22 +56,20 @@ BOARD_USES_METADATA_PARTITION := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 PLATFORM_VERSION := 127
 PLATFORM_SECURITY_PATCH := 2127-12-31
-
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_DEVICE_MODULES += libion libandroidicu vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
-
-#
 TARGET_USES_MKE2FS := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
+# Libraries
+TARGET_RECOVERY_DEVICE_MODULES += libion libandroidicu vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
