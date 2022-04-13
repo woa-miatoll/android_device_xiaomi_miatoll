0d10aacb79c85318e1e9f211dc053a3e3865c686
Darth9 <reyprimerjefe@gmail.com>
2022-04-13 11:07:05 +0100

    Ensure that /sdcard is bind-unmounted before f2fs data repair or format

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -82,6 +82,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# run a process after formatting data to recreate /data/media/0 (only when forced-encryption is being disabled)
 	export OF_RUN_POST_FORMAT_PROCESS=1
 
+	# ensure that /sdcard is bind-unmounted before f2fs data repair or format
+	export OF_UNBIND_SDCARD_F2FS=1
+
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
   	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
