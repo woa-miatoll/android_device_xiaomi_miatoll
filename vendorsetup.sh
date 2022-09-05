19b86f573269738146833b9b307c308f7e307315
Darth9 <reyprimerjefe@gmail.com>
2022-09-05 15:50:08 +0100

    Restore patching of avb20 to FBEv1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -58,7 +58,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_XZ_UTILS=1
 	export OF_ENABLE_LPTOOLS=1
         export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	#export OF_PATCH_AVB20=1
         export FOX_DELETE_AROMAFM=1
         export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
         export FOX_USE_NANO_EDITOR=1
@@ -92,9 +91,11 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	   export FOX_VARIANT="unwrap"
 	fi
 
-	# flashlight
+	# FBEv2
 	if [ "$FOX_VARIANT" = "A12_FBEv2" ]; then
 	   export OF_FLASHLIGHT_ENABLE=0
+	else
+	   export OF_PATCH_AVB20=1
 	fi
 
 	# let's see what are our build VARs
