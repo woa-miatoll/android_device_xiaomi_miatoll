185729674778fa8c29f73711948a62186c20df7a
Darth9 <reyprimerjefe@gmail.com>
2023-01-13 10:10:41 +0000

    Update the copyright notices

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -1,7 +1,26 @@
 #!/system/bin/sh
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2023 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+#
+
 # This script is needed to automatically set device props.
-
-
 load_curtana()
 {
     resetprop "ro.product.model" "Redmi Note 9S"
